f69a8039338db21db77268ade77b37d80a09d734
kangmingfa <1640528278@qq.com>
2024-06-22 14:11:05 +0800

    Reuse logger set/get api. Add dynamic change bpf log dump space.
    Signed-off-by: kangmingfa <1640528278@qq.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -33,6 +33,8 @@ const (
 	BPF_LOG_WARN  = 1
 	BPF_LOG_INFO  = 2
 	BPF_LOG_DEBUG = 3
+	BPF_DUMP_SPACE_USERSPACE = 1 << 2
+	BPF_DUMP_SPACE_TRACE_PIPE = 1 << 3
 
 	// Ip(0.0.0.2 | ::2) used for control command, e.g. KmeshControl | ByPass
 	ControlCommandIp4 = "0.0.0.2"
